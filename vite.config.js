6f503dd1b0023062203f5f96d88361477261ca40
Urban1997 <155580062+Urban1997@users.noreply.github.com>
2025-09-07 19:11:02 -0600

    Fix rutas para GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,18 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import tailwindcss from '@tailwindcss/vite'
 
-
 export default defineConfig({
-  plugins: [react(), tailwindcss(),],
+  // 👇 Necesario para GitHub Pages
+  base: '/Mi_Bautizo_Ambar_Lucia/',
+
+  plugins: [react(), tailwindcss()],
+
   optimizeDeps: {
-    include: ['react', 'react-dom', 'react-router-dom', 'pdfjs-dist/build/pdf.worker.entry'],
-  }
+    include: [
+      'react',
+      'react-dom',
+      'react-router-dom',
+      'pdfjs-dist/build/pdf.worker.entry'
+    ],
+  },
 })
